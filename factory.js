5f7c2b313ad22c6fd6c9bfb1160be3775eddb14b
Eagan Martin <mece.martinece@gmail.com>
2018-11-17 10:58:40 +0800

    Added implementation of factory pattern;

---
+++ factory.js
@@ -0,0 +1,49 @@
+/**
+ * Supplier Model Classes
+ */
+class Magnolia {
+	supplyMeat() {
+		console.log('Chicken Meat')
+	}
+}
+
+class Monterey {
+	supplyMeat() {
+		console.log('Pork Meat')
+	}
+}
+
+/**
+ * General Model Class
+ */
+class Supplier {
+	takeOrders() {
+		const supplier = this.makeSupplier()
+		supplier.supplyMeat()
+	}
+}
+
+/**
+ * Implementation of classes
+ */
+class ChickenMeatSupplier extends Supplier {
+	makeSupplier() {
+		return new Magnolia()
+	}
+}
+
+class PortMeatSupplier extends Supplier {
+	makeSupplier() {
+		return new Monterey()
+	}
+}
+
+/**
+ * Usage
+ * @type {ChickenMeatSupplier}
+ */
+const chickenMeatSupplier = new ChickenMeatSupplier()
+chickenMeatSupplier.takeOrders()
+
+const porkMeatSupplier = new PorkMeatSupplier()
+porkMeatSupplier.takeOrders()
