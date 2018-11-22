eabbc3d03d702c6584b73dc9a1431de8b198ba39
Eagan Martin <mece.martinece@gmail.com>
2018-11-22 11:36:42 +0800

    Added implements class to simulate interface classes;

---
+++ implements.js
@@ -0,0 +1,40 @@
+/**
+ * Implements function to simulate implementing of interface classes
+ * @param {[type]} implementer.pseudoInterface [description]
+ */
+function Implements(implementer, pseudoInterface) {
+	for(property in pseudoInterface) {
+		if(typeof pseudoInterface[property] === "function")
+			if(typeof implementer[property] != "function")
+				throw new Implements.functionNotImplemented(
+						implementer.constructor.name,
+						property,
+						pseudoInterface.constructor.name
+					);
+	}
+}
+
+/**
+ * [functionNotImplemented description]
+ * @param  {[type]} implementing    [description]
+ * @param  {[type]} implemented     [description]
+ * @param  {[type]} pseudoInterface [description]
+ * @return {[type]}                 [description]
+ */
+Implements.functionNotImplemented = function(implementing, implemented, pseudoInterface) {
+	this.implementing = implementing;
+	this.implemented = implemented;
+	this.pseudoInterface = pseudoInterface;
+}
+
+/**
+ * [message description]
+ * @return {[type]} [description]
+ */
+Implements.functionNotImplemented.prototype.message = function() {
+	return this.implementing + " does not implement function: " + this.implemented;
+}
+
+module.exports = {
+	Implements: Implements
+}
